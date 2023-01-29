f92ab417f25f9a7614671efaaa167aa8efa91419
Rene Markovič <41106629+ReneMarkovic@users.noreply.github.com>
2023-01-29 15:59:34 +0100

    update

--- ocr.py
+++ ocr.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C://Program Files//Tesseract-OCR//tesseract.exe'
+#pytesseract.pytesseract.tesseract_cmd = r'C://Program Files//Tesseract-OCR//tesseract.exe'
 
 # Step 1: Allow the user to select an image file
 image_file = st.file_uploader("Upload an image file", type=["jpg", "png", "jpeg"])
@@ -10,7 +10,7 @@ if image_file is not None:
     # Step 2: Open the image file and convert it to a format Pytesseract can read
     image = Image.open(image_file)
     # Step 3: Perform OCR on the image and extract the text
-    text = pytesseract.image_to_string(image, lang="slv")
+    text = pytesseract.image_to_string(image)
 
     # Step 4: Display the extracted text on the webpage
     st.write("Extracted text:")
